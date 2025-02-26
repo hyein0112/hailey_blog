69a32ba343096aa9dfc8cb4b64092afe4f798e7f
hyein0112 <s21046@gsm.hs.kr>
2025-02-26 17:08:13 +0900

    feat: 홈페이지 재검증 로직 추가

--- app/api/write/route.ts
+++ app/api/write/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "../util/database";
+import { revalidatePath } from "next/cache";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     db.collection("posts").insertOne(body);
+    revalidatePath("/");
     return NextResponse.json(body, { status: 200 });
   } catch (e) {
     console.error(e);
