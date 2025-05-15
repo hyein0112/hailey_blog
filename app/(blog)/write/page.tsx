d7c88a51d3136f754f6847ea1fa8a8f8f314fc16
hyein0112 <s21046@gsm.hs.kr>
2025-05-15 14:38:54 +0900

    feat: 포스트 등록 후 페이지 replace

--- app/(blog)/write/page.tsx
+++ app/(blog)/write/page.tsx
@@ -24,7 +24,7 @@ export default function WritePage() {
         thumbnail: postData?.thumbnail,
         createdAt: dayjs.tz().format("YYYY-MM-DD HH:mm:ss"),
       });
-      console.log(data);
+      router.replace("/blog");
     } catch (e) {
       console.error(e);
     }
@@ -46,7 +46,7 @@ export default function WritePage() {
       <S.Container>
         <MyEditor />
         <S.BottomButtonBox>
-          <Link href={"/"}>
+          <Link href={"/blog"}>
             <S.BottomButton isSubmit={false}>취소</S.BottomButton>
           </Link>
           <S.BottomButton onClick={handleClickPost} isSubmit={true}>
