73b406f92d6f4d7fede4e54d3834ea3e6f698f00
Hailey Baek <81304598+hyein0112@users.noreply.github.com>
2025-06-29 16:13:42 +0900

    data update

--- app/(blog)/about/data/studyList.ts
+++ app/(blog)/about/data/studyList.ts
@@ -1,21 +1,31 @@
 export const studyList = [
   {
     id: 0,
+    title: "한 달 만에 만든 사내 서비스",
+    link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/%E1%84%8F%E1%85%B5%E1%84%85%E1%85%B5%E1%86%BC%20%E1%84%87%E1%85%A1%E1%86%AF%E1%84%91%E1%85%AD%E1%84%8C%E1%85%A1%E1%84%85%E1%85%AD.pdf",
+  },
+  {
+    id: 1,
+    title: "바이브코딩",
+    link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/Vibe%20Coding%20%E1%84%83%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%92%E1%85%A2%E1%84%87%E1%85%A9%E1%84%80%E1%85%B5.pdf",
+  },
+  {
+    id: 2,
     title: "Docker 기초",
     link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/docker_for_FE.pdf",
   },
   {
-    id: 1,
+    id: 3,
     title: "디바운싱과 쓰로틀링",
     link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/debouncing_throttling.pdf",
   },
   {
-    id: 2,
+    id: 4,
     title: "정규식으로 퍼지검색",
     link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/fuzzy_string_search.pdf",
   },
   {
-    id: 3,
+    id: 5,
     title: "부동소수점",
     link: "https://pub-24c7455649c447158e9e42357ec70220.r2.dev/hailey-blog/%E1%84%87%E1%85%AE%E1%84%83%E1%85%A9%E1%86%BC%E1%84%89%E1%85%A9%E1%84%89%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%B7.pdf",
   },
